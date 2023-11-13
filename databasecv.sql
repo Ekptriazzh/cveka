4eb05aa7a8db775a25ad2da48f49f9c138f5c9ab
eka putri <ekaputrinur1201@gmail.com>
2023-11-13 20:16:51 +0700

    first commit

---
+++ databasecv.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE curikulumeka;
+USE curikulumeka;
+CREATE TABLE curikulum_eka (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nama VARCHAR(100) NOT NULL,
+	alamat TEXT NOT NULL,
+	telepon VARCHAR(20) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	web VARCHAR(255) NOT NULL, 
+	pendidikan TEXT NOT NULL,
+	pengalaman_kerja TEXT NOT NULL,
+	keterampilan TEXT NOT NULL,
+	foto_path VARCHAR(255) NOT NULL
+);
+SELECT*FROM curiculum_eka;
